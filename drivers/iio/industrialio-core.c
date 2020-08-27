6a2f7f418ae99fac752299d0325f98b0e0056e95
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-08-27 08:24:27 +0200

    iio: industrialio-core.c: solve a kernel-doc warning
    kernel-doc currently compains with:
    	./drivers/iio/industrialio-core.c:177: warning: Function parameter or member 'indio_dev' not described in 'iio_get_debugfs_dentry'
    	./drivers/iio/industrialio-core.c:1530: warning: Function parameter or member 'parent' not described in 'iio_device_alloc'
    
    The problem is that the comment just before iio_get_debugfs_dentry()
    is just a comment, and not really a kernel-doc markup.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -169,7 +169,7 @@ static const char * const iio_chan_info_postfix[] = {
 };
 
 #if defined(CONFIG_DEBUG_FS)
-/**
+/*
  * There's also a CONFIG_DEBUG_FS guard in include/linux/iio/iio.h for
  * iio_get_debugfs_dentry() to make it inline if CONFIG_DEBUG_FS is undefined
  */
